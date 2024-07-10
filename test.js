ce4cf5ed8a3f20763c19da260714ca48afd7bf81
hViNh12 <118531652+hViNh12@users.noreply.github.com>
2024-07-10 14:31:03 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+// script.js
+
+document.getElementById('docs-button-link').addEventListener('click', function() {
+  window.location.href = 'https://demo.htmlhunters.com/shopy/documentation.html';
+});
